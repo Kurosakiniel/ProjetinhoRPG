b0f0c054eec853c3c9a680338816586756aae4b3
Kurosakiniel <ichigoniel@gmail.com>
2026-09-08 10:49:56 -0300

    adicionado pastas para medias e primeiro modeos feito

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Ficha(models.Model):
+    # atributos base
+    name = models.CharField(max_length=50)
+    profissao = models.CharField(max_length=50)
+    vida = models.IntegerField(default=0)
+    forca = models.IntegerField(default=0)
+    destreza = models.IntegerField(default=0)
+    agilidade = models.IntegerField(default=0)
+    inteligencia = models.IntegerField(default=0)
+    sabedoria = models.IntegerField(default=0)
+    carisma = models.IntegerField(default=0)
+    labia = models.IntegerField(default=0)
+    percepcao = models.IntegerField(default=0)
+    intuicao = models.IntegerField(default=0)
+    fe = models.IntegerField(default=0)
+
+    #status especias
+    status_especiais = models.TextField(blank=True, null=True)
+
+    #status religioso
+    status_religiosos = models.TextField(blank=True, null=True)
+        
+    #religiao
+    religiao = [
+        ("Hibirismo", "Hibirismo"),
+        ("Ateismo", "Ateismo"),
+        ("Agnosticismo", "Agnosticismo"),
+        ("Flowdiesto", "Flowdiesto"),
+        ("Zudelista", "Zudelista"),
+        ("Sanhismo", "Sanhismo"),
+        ("Cristianismo", "Cristianismo")
+    ]
